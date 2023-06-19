4977d6f0499b5335e0b7b5bdcd21687e04e8e3db
Vitalii Dududka <vitalyd_2001@yahoo.com>
2023-06-19 23:22:29 +0200

    JS 09 task 01
    completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+const bodyBackgroundColor = document.querySelector("body");
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+
+let timerId = null;
+
+startBtn.addEventListener('click', onStartClick);
+
+function onStartClick() {
+
+    timerId = setInterval(() => {
+    bodyBackgroundColor.style.backgroundColor = getRandomHexColor();
+    }, 1000)
+    startBtn.disabled = true;
+}
+
+stopBtn.addEventListener('click', onStopClick);
+
+function onStopClick() {
+    clearInterval(timerId);
+    startBtn.disabled = false;
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
 
